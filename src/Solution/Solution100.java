ba9001d8e0dffe64b649c80ad771bfac41983d72
Sage-Y9 <1131391437@qq.com>
2022-02-08 17:23:44 +0800

    100. 相同的树

---
+++ src/Solution/Solution100.java
@@ -0,0 +1,21 @@
+package Solution;
+
+import DataStructure.TreeNode;
+
+/**
+ * @author yangjk
+ * @date 2022/2/8 17:20
+ */
+public class Solution100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}

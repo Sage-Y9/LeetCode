f7544cf3082cc2c4a711e1a0818eff5a9a33071c
Sage-Y9 <1131391437@qq.com>
2022-02-09 09:50:33 +0800

    704. 二分查找

---
+++ src/Solution/Solution704.java
@@ -0,0 +1,27 @@
+package Solution;
+
+/**
+ * @author yangjk
+ * @date 2022/2/9 09:35
+ */
+public class Solution704 {
+    public static int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        System.out.println(search(nums, 9));
+    }
+}
